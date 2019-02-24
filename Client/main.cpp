6b842f33fa7bc6e41feaaa5a069c024c3111ee17
Joern Teuber <joern.teuber@cs.uni-bremen.de>
2019-02-24 20:45:25 +0100

    Make the client asynchronous as well.

--- Client/main.cpp
+++ Client/main.cpp
@@ -7,27 +7,55 @@ int main()
 {
 	try
 	{
-		// Connect and give it some time to connect
-		asio::ip::tcp::iostream stream("localhost", "12345");
-		stream.expires_from_now(std::chrono::seconds(60));
+		// create the context and things necessary to connect
+		asio::io_context ioContext;
+		asio::ip::tcp::socket socket( ioContext );
+		asio::ip::tcp::endpoint endpoint( asio::ip::address_v4::from_string("127.0.0.1"), 12345 );
 
-		// Send the message
-		stream << "Hello World!\r\n";
+		// create a buffer to hold the received server status in
+		asio::streambuf buffer;
 
-		// and then receive the confirmation
-		std::string status;
-		while( std::getline(stream, status) )
-			std::cout << status << "\n";
-		std::cout << stream.rdbuf();
-
-		// check for any errors
-		if( !stream )
+		socket.async_connect( endpoint, [&] (const asio::error_code& error)
 		{
-			std::cout << "Socket error: " << stream.error().message() << std::endl;
-			return 1;
-		}
+			// check for any errors while connecting
+			if( error )
+			{
+				std::cout << "ASIO error: " << error.message() << std::endl;
+				return;
+			}
 
-		// we don't need to close the stream explicitly as that is done by the destructor when it's going out if scope
+			// Send the message
+			std::string message = "Hello World!\n";
+			socket.async_write_some(asio::buffer(message), [&] (const asio::error_code& error, std::size_t)
+			{
+				// check for any errors while sending
+				if( error )
+				{
+					std::cout << "ASIO error: " << error.message() << std::endl;
+					return;
+				}
+
+				// and then receive the confirmation
+				asio::async_read_until( socket, buffer, '\n',
+				                          [&] (const asio::error_code& error, std::size_t /*bytesReceived*/)
+				{
+					// check for any errors while receiving
+					if( error )
+					{
+						std::cout << "ASIO error: " << error.message() << std::endl;
+						return;
+					}
+
+					// and display it
+					std::cout << &buffer;
+					std::cout.flush(); // just to be sure to stay up-to-date
+				} );
+			} );
+		} );
+
+
+		// let the context take care of the event loop while stuff is being sent and received
+		ioContext.run();
 	}
 	catch (std::exception& e)
 	{
