6d48fffe1b3448f4aca568de23801bf79c5e5007
Joern Teuber <joern.teuber@cs.uni-bremen.de>
2019-02-24 17:18:51 +0100

    Write a very simple synchronous client and server to get my bearings in asio.

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 
-using namespace std;
+#include <asio.hpp>
 
+// CLIENT
 int main()
 {
-	cout << "Hello World!" << endl;
+	try
+	{
+		// Connect and give it some time to connect
+		asio::ip::tcp::iostream stream("localhost", "12345");
+		stream.expires_from_now(std::chrono::seconds(60));
+
+		// Send the message
+		stream << "Hello World!\r\n";
+
+		// and then receive the confirmation
+		std::string status;
+		while( std::getline(stream, status) )
+			std::cout << status << "\n";
+		std::cout << stream.rdbuf();
+
+		// check for any errors
+		if( !stream )
+		{
+			std::cout << "Socket error: " << stream.error().message() << std::endl;
+			return 1;
+		}
+
+		// we don't need to close the stream explicitly as that is done by the destructor when it's going out if scope
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
 	return 0;
 }
--- Server/main.cpp
+++ Server/main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
 
-using namespace std;
+#include <asio.hpp>
 
+// SERVER
 int main()
 {
-	cout << "Hello World!" << endl;
+	try
+	{
+		// create the context and socket as well as an acceptor on which we will be accepting incoming connections
+		asio::io_context ioContext;
+		asio::ip::tcp::acceptor acceptor(ioContext, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 12345));
+		asio::ip::tcp::socket socket(ioContext);
+
+		// block until we get a connection
+		acceptor.accept(socket);
+
+		// read some data from the clinet
+		asio::error_code errorCode;
+		std::array<char, 128> buffer;
+		size_t messageLength = socket.read_some(asio::buffer(buffer), errorCode);
+
+		// check for any errors while receiving
+		if( errorCode )
+		{
+			std::cout << "ASIO error: " << errorCode.message() << std::endl;
+			return 1;
+		}
+
+		// and display it
+		std::cout.write(buffer.data(), messageLength);
+
+
+		// now send a status message to the client
+		std::string message = "ack\n";
+		asio::write(socket, asio::buffer(message), errorCode);
+
+		// check for any errors while sending
+		if( errorCode )
+		{
+			std::cout << "ASIO error: " << errorCode.message() << std::endl;
+			return 1;
+		}
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
 	return 0;
 }
